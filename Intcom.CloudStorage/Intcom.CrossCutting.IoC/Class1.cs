e4ea5afc1a52fecc2b1a2417af4861aa722e2c3d
Daniel Canedo <danielcanedo@live.com>
2017-05-24 10:16:09 -0300

    Initial Skeleton

---
+++ Intcom.CloudStorage/Intcom.CrossCutting.IoC/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intcom.CrossCutting.IoC
+{
+    public class Class1
+    {
+    }
+}
